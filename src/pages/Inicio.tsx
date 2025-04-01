e2970b4475453c64e1e8dffe90d4de703331a5a4
Enrique Rojas <rojasguerrero123@gmail.com>
2025-03-31 19:43:07 -0600

    ptm

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -4,7 +4,6 @@ import { jwtDecode } from "jwt-decode";
 import { useEffect, useState } from "react";
 
 interface UserData {
-  email?: string;
   grado?: string;
   username?: string;
   grupo?: string;
@@ -41,17 +40,51 @@ const Inicio = () => {
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100 vw-100 bg-dark">
-      <h1 className="text-center text-white">Bienvenido a la página de inicio</h1>
-      <p className="text-center text-white">Esta es la página de inicio de tu aplicación.</p>
+      <h1 className="text-center text-white">
+        Bienvenido a la página de inicio
+      </h1>
+      <p className="text-center text-white">
+        Bienvenido a la Plataforma de Gestión y Monitoreo de APIs
+      </p>
+      <p className="text-justify text-white">
+        Esta aplicación permite gestionar y analizar el uso de dos servidores
+        RESTful, diseñados para autenticar usuarios mediante JWT con
+        autenticación multifactor (MFA), almacenar registros detallados de
+        actividad (logs) y evaluar la seguridad y eficiencia del sistema.
+      </p>
+      <p className="text-justify text-white">
+        <strong>Funciones principales:</strong>
+      </p>
+      <p className="text-justify text-white">
+        Registro y autenticación segura con MFA, consulta de información sobre
+        la versión del servidor y datos del usuario, registro detallado de logs
+        con análisis gráfico, comparación entre un servidor con restricciones de
+        seguridad (Rate Limit) y otro sin ellas. Puedes acceder al registro de
+        actividad en la sección de Logs, donde se presentan gráficos
+        comparativos entre los dos servidores.
+      </p>
       {userData && (
         <div className="text-white text-center">
-          {userData.username && <p><strong>Usuario:</strong> {userData.username}</p>}
-          {userData.email && <p><strong>Correo:</strong> {userData.email}</p>}
-          {userData.grado && <p><strong>Grado:</strong> {userData.grado}</p>}
-          {userData.grupo && <p><strong>Grupo:</strong> {userData.grupo}</p>}
+          {userData.grado && (
+            <p>
+              <strong>{userData.grado} </strong> {userData.username}
+            </p>
+          )}
+          {userData.grupo && (
+            <p>
+              <strong>Grupo:</strong> {userData.grupo}
+            </p>
+          )}
         </div>
       )}
-      <button className="btn btn-primary" onClick={goToLogs}>Ir a Logs</button>
+      <button className="btn btn-primary" onClick={goToLogs}>
+        Ir a Logs
+      </button>
+
+      <div className="logo-container">
+        <img src="../../public/vite.svg" alt="Vite Logo" className="logo" />
+        <img src="../assets/react.svg" alt="React Logo" className="logo" />
+      </div>
 
       <div
         style={{
