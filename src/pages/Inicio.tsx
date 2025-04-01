a1cec195ebd4cfe3ee82d62189dc786dc1d90f21
Enrique Rojas <rojasguerrero123@gmail.com>
2025-03-31 19:30:42 -0600

    mas pal jwt

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,8 +1,34 @@
 import { useNavigate } from "react-router-dom";
 import { IconLogout2 } from "@tabler/icons-react";
+import { jwtDecode } from "jwt-decode";
+import { useEffect, useState } from "react";
+
+interface UserData {
+  email?: string;
+  grado?: string;
+  username?: string;
+  grupo?: string;
+}
 
 const Inicio = () => {
   const navigate = useNavigate();
+  const [userData, setUserData] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+    if (token) {
+      try {
+        const decoded = jwtDecode<UserData>(token);
+        setUserData(decoded);
+      } catch (error) {
+        console.error("Error al decodificar el token", error);
+        localStorage.removeItem("jwtToken");
+        navigate("/login");
+      }
+    } else {
+      navigate("/login");
+    }
+  }, [navigate]);
 
   const goToLogs = () => {
     navigate("/logs");
@@ -15,16 +41,17 @@ const Inicio = () => {
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100 vw-100 bg-dark">
-      <h1 className="text-center text-white">
-        Bienvenido a la página de inicio
-      </h1>
-      <p className="text-center text-white">
-        Esta es la página de inicio de tu aplicación.
-      </p>
-      <a className="btn btn-primary" onClick={goToLogs}>
-        {" "}
-        Ir a Logs
-      </a>
+      <h1 className="text-center text-white">Bienvenido a la página de inicio</h1>
+      <p className="text-center text-white">Esta es la página de inicio de tu aplicación.</p>
+      {userData && (
+        <div className="text-white text-center">
+          {userData.username && <p><strong>Usuario:</strong> {userData.username}</p>}
+          {userData.email && <p><strong>Correo:</strong> {userData.email}</p>}
+          {userData.grado && <p><strong>Grado:</strong> {userData.grado}</p>}
+          {userData.grupo && <p><strong>Grupo:</strong> {userData.grupo}</p>}
+        </div>
+      )}
+      <button className="btn btn-primary" onClick={goToLogs}>Ir a Logs</button>
 
       <div
         style={{
